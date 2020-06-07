cdf02b29197743e329a6da370a5680f5f7ba9ebc
Brian Yamasaki <brian.yamasaki@gmail.com>
2020-06-07 16:13:34 -0700

    better handling for cookies

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,7 +1,15 @@
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
 import { app } from '../app';
+import request from 'supertest';
 
+declare global {
+  namespace NodeJS {
+    interface Global {
+      signin(): Promise<string[]>;
+    }
+  }
+}
 let mongo: any;
 beforeAll(async () => {
   process.env.JWT_KEY = 'this is just a test';
@@ -25,3 +33,19 @@ afterAll(async () => {
   await mongo.stop();
   await mongoose.connection.close();
 });
+
+global.signin = async () => {
+  const email = 'test@test.com';
+  const password = 'password';
+
+  const response = await request(app)
+    .post('/api/users/signup')
+    .send({
+      email,
+      password,
+    })
+    .expect(201);
+
+  const cookie = response.get('Set-Cookie');
+  return cookie;
+};
